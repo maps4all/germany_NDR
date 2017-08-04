e624b248624026c610174d47bde07312cfa947c3
maps4all <maps4all007@yandex.com>
2017-08-05 00:29:57 +0200

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,7 @@
+
+{
+    "moduleId" : "grabowCommuter/germany_NDR",
+    "type" : "Rss",
+    "name" : "NDR",
+    "uri" : "http://www.ndr.de/nachrichten/verkehr/feed105-verkehr_region-nord_format-rss.xml"
+}
